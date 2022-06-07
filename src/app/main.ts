4cd62947e0de860742e1c1a970bee40e1c52b061
mclaralvs <mclaralvs@gmail.com>
2022-06-07 15:13:03 -0300

    Exclusão de Clientes funcionando.

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,7 +5,8 @@ import CadastroProduto from "../negocio/cadastroProduto";
 import CadastroServico from "../negocio/cadastroServico";
 import ListagemClientes from "../negocio/listagemClientes";
 import ListagemProdutos from "../negocio/listagemProdutos";
-import ListagemServicos from "../negocio/listagemServicos"
+import ListagemServicos from "../negocio/listagemServicos";
+import ExcluirCliente from "../negocio/excluirCliente"
 
 console.log(`Bem-vindo ao cadastro de clientes do Grupo World Beauty`)
 let empresa = new Empresa()
@@ -15,10 +16,11 @@ while (execucao) {
     console.log(`Opções:`);
     console.log(`1 - Cadastrar cliente`);
     console.log(`2 - Listar todos os clientes`);
-    console.log(`3 - Cadastrar produto`);
-    console.log(`4 - Listar todos os produtos`);
-    console.log(`5 - Cadastrar produto`);
-    console.log(`6 - Listar todos os produtos`);
+    console.log(`3 - Excluir um cliente`);
+    console.log(`4 - Cadastrar produto`);
+    console.log(`5 - Listar todos os produtos`);
+    console.log(`6 - Cadastrar produto`);
+    console.log(`7 - Listar todos os produtos`);
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -34,21 +36,25 @@ while (execucao) {
             listagem.listar()
             break;
         case 3:
+            let excluir = new ExcluirCliente(empresa.getClientes)
+            excluir.excluir()
+            break;
+        case 4:
             let cadastroProduto = new CadastroProduto(empresa.getProdutos)
             cadastroProduto.cadastrar()
             break;
-        case 4:
+        case 5:
             let listagemProdutos = new ListagemProdutos(empresa.getProdutos)
             listagemProdutos.listar()
             break;
-        case 5:
+        case 6:
             let cadastroServico = new CadastroServico(empresa.getServicos)
             cadastroServico.cadastrar()
             break;
-        case 6:
+        case 7:
             let listagemServicos = new ListagemServicos(empresa.getServicos)
             listagemServicos.listar()
-            break;    
+            break;
         case 0:
             execucao = false
             console.log(`Até mais`)
---
+++ src/negocio/excluirCliente.ts
@@ -0,0 +1,25 @@
+import Cliente from "../modelo/cliente"
+import Entrada from "../io/entrada";
+import Excluir from "./excluir";
+
+export default class ExcluirCliente extends Excluir {
+    private clientes: Array<Cliente>
+    private entrada: Entrada
+    constructor(clientes: Array<Cliente>) {
+        super()
+        this.clientes = clientes
+        this.entrada = new Entrada()
+    }
+    public excluir(): void {
+        var cpf = this.entrada.receberTexto('Insira o CPF de quem quer excluir: ')
+        
+        var excluirCliente = (cpf) => {
+            for (let index = 0; index < this.clientes.length; index++) {
+                if (this.clientes[index].getCpf.getValor == cpf) {
+                    this.clientes.splice(index, 1)
+                }
+            }
+        }
+        excluirCliente(cpf)
+    }
+}
